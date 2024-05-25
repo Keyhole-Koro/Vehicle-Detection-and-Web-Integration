5c883946010bc0931332cdd56b7d97a6418ad399
Keyhole-Koro <130579219+Keyhole-Koro@users.noreply.github.com>
2024-05-25 18:54:55 +0900

    car detection works

---
+++ detect.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SAMPLES_DIR="./samples"
+
+CONF_THRESHOLD=${1:-0.5}
+NMS_THRESHOLD=${2:-0.4}
+
+for IMAGE_PATH in "$SAMPLES_DIR"/*.png; do
+    echo "Processing $IMAGE_PATH..."
+    python3 ./detect_cars.py "$IMAGE_PATH" $CONF_THRESHOLD $NMS_THRESHOLD
+done
